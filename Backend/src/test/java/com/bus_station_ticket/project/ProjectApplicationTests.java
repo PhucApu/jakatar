83a029e6f663cf53c5745da16ace19277eeaf23d
PhucApu <phucapu056@gmail.com>
2024-09-24 10:42:10 +0700

    project structure

---
+++ Backend/src/test/java/com/bus_station_ticket/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bus_station_ticket.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
