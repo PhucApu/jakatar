c72413f0afae38d59075610dddbda03c3931d087
thinh tu <khangthinhproc@gmail.com>
2024-09-28 13:56:19 +0700

    frontend initial push

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+import { content, plugin } from "flowbite-react/tailwind";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    content(),
+  ],
+  plugins: [
+    plugin(),
+  ],
+};
---
+++ Frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
