2c494fc4252c3312263dff3a48dfef0c2312a5ad
DungTriNguyen <dungtringuyen113@gmail.com>
2024-10-10 04:23:33 +0700

    Update AllRoute.AllRoutejsx and Ticket.Ticketjsx

--- Frontend/src/pages/Ticket.jsx
+++ Frontend/src/pages/Ticket.jsx
@@ -1,5 +1,43 @@
 export default function Ticket() {
   return (
-    <div>Tra cứu vé</div>
+    <section className="py-24">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+          <div className="lg:mb-0 mb-10">
+            <div className="group w-full h-full">
+              <div className="relative h-full">
+                <img
+                  src="/hihi.png"
+                  alt="Ticket Information"
+                  className="w-full h-full lg:rounded-l-2xl rounded-2xl bg-blend-multiply bg-cyan-700 object-cover"
+                />
+                <h1 className="font-manrope text-white text-4xl font-bold leading-10 absolute top-11 left-11">
+                  Tra cứu vé
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-gray-50 p-5 lg:p-11 lg:rounded-r-2xl rounded-2xl">
+            <h2 className="text-cyan-600 font-manrope text-4xl font-semibold leading-10 mb-11">
+              Nhập thông tin vé
+            </h2>
+            <input
+              type="text"
+              className="w-full h-12 text-gray-600 placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 rounded-full border border-gray-200 focus:outline-none pl-4 mb-10"
+              placeholder="Số điện thoại"
+            />
+            <input
+              type="text"
+              className="w-full h-12 text-gray-600 placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 rounded-full border border-gray-200 focus:outline-none pl-4 mb-10"
+              placeholder="Mã vé xe"
+            />
+            <button className="w-full h-12 text-white text-base font-semibold leading-6 rounded-full transition-all duration-700 hover:bg-cyan-800 bg-cyan-600 shadow-sm">
+              Tra cứu thông tin
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
