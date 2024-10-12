452c852603439a1384d3161ec510fcee023b0190
thinh tu <khangthinhproc@gmail.com>
2024-10-12 16:21:22 +0700

    update font

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', flowbite.content()],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [flowbite.plugin()],
 };
