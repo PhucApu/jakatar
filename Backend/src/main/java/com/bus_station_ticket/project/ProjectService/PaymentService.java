4a6da5320c867badf04c2a1d920ba8602e9f994f
PhucApu <phucapu056@gmail.com>
2024-11-07 23:22:51 +0700

    update 07/11/2024

--- Backend/src/main/java/com/bus_station_ticket/project/ProjectService/PaymentService.java
+++ Backend/src/main/java/com/bus_station_ticket/project/ProjectService/PaymentService.java
@@ -2,9 +2,13 @@ package com.bus_station_ticket.project.ProjectService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bus_station_ticket.project.ProjectDTO.PaymentDTO;
 import com.bus_station_ticket.project.ProjectEntity.PaymentEntity;
@@ -12,7 +16,7 @@ import com.bus_station_ticket.project.ProjectMappingEntityToDtoSevice.PaymentMap
 import com.bus_station_ticket.project.ProjectRepository.PaymentRepo;
 
 @Service
-public class PaymentService {
+public class PaymentService implements SimpleServiceInf<PaymentEntity, PaymentDTO, Long> {
 
        @Autowired
        private PaymentRepo repo;
@@ -23,7 +27,8 @@ public class PaymentService {
        // Lấy một đối tượng PaymentEntity theo giá trị
        // Input: paymentId (Long)
        // Output: PaymentEntity có giá trị paymentId tương ứng
-
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
        public PaymentEntity getById(Long paymentId) {
               return this.repo.findByPaymentId(paymentId).orElse(null);
        }
@@ -31,7 +36,8 @@ public class PaymentService {
        // Mapping đối tượng PaymentEntity --> PaymentDTO
        // Input: paymentId (Long)
        // Output: PaymentDTO có giá trị paymentId tương ứng
-
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
        public PaymentDTO getById_toDTO(Long paymentId) {
               PaymentEntity paymentEntity = this.repo.findByPaymentId(paymentId).orElse(null);
 
@@ -44,13 +50,15 @@ public class PaymentService {
        // Lấy tất cả các đối tượng PaymentEntity
        // Input:
        // Output: List
-
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
        public List<PaymentEntity> getAll() {
               return this.repo.findAll();
        }
 
        // Mapping đối tượng List<PaymentEntity> --> List<PaymentDTO>
-
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
        public List<PaymentDTO> getAll_toDTO() {
               List<PaymentEntity> listPaymentEntities = this.repo.findAll();
 
@@ -64,4 +72,91 @@ public class PaymentService {
               }
               return listPaymentDTOs;
        }
+
+       @Override
+       public Boolean delete(Long id) {
+
+              // kierm tra
+              Optional<PaymentEntity> optional = this.repo.findByPaymentId(id);
+
+              // neu co kq
+              if (optional.isPresent()) {
+                     // xoa
+                     this.repo.delete(optional.get());
+                     return true;
+              }
+
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
+       @Override
+       public Boolean save(PaymentEntity entityObj) {
+              // kierm tra
+              Optional<PaymentEntity> optional = this.repo.findByPaymentId(entityObj.getPaymentId());
+
+              // neu co kq
+              if (optional.isPresent() == false) {
+                     // them
+                     this.repo.save(entityObj);
+                     return true;
+              }
+
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
+       @Override
+       public Boolean save_toDTO(PaymentDTO dtoObj) {
+              // kierm tra
+              Optional<PaymentEntity> optional = this.repo.findByPaymentId(dtoObj.getPaymentId());
+
+              // neu kq khong co
+              if (optional.isPresent() == false) {
+                     // mapping
+                     PaymentEntity paymentEntity = this.paymentMapping.toEntity(dtoObj);
+
+                     // them
+                     this.repo.save(paymentEntity);
+                     return true;
+              }
+
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ, rollbackFor = Exception.class)
+       @Override
+       public Boolean update(PaymentEntity entityObj) {
+
+              // kierm tra
+              Optional<PaymentEntity> optional = this.repo.findByPaymentId(entityObj.getPaymentId());
+
+              if (optional.isPresent()) {
+                     // sua
+                     this.repo.save(optional.get());
+                     return true;
+              }
+
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ, rollbackFor = Exception.class)
+       @Override
+       public Boolean update_toDTO(PaymentDTO dtoObj) {
+              // kierm tra
+              Optional<PaymentEntity> optional = this.repo.findByPaymentId(dtoObj.getPaymentId());
+
+              if (optional.isPresent()) {
+
+                     // mapping
+                     PaymentEntity paymentEntity = this.paymentMapping.toEntity(dtoObj);
+
+                     // sua
+                     this.repo.save(paymentEntity);
+                     return true;
+              }
+
+              return false;
+       }
+
 }
--- Backend/src/main/java/com/bus_station_ticket/project/ProjectService/PenaltyTicketService.java
+++ Backend/src/main/java/com/bus_station_ticket/project/ProjectService/PenaltyTicketService.java
@@ -2,9 +2,13 @@ package com.bus_station_ticket.project.ProjectService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bus_station_ticket.project.ProjectDTO.PenaltyTicketDTO;
 import com.bus_station_ticket.project.ProjectEntity.PenaltyTicketEntity;
@@ -12,31 +16,32 @@ import com.bus_station_ticket.project.ProjectMappingEntityToDtoSevice.PenaltyTic
 import com.bus_station_ticket.project.ProjectRepository.PenaltyTicketRepo;
 
 @Service
-public class PenaltyTicketService {
-       
+public class PenaltyTicketService implements SimpleServiceInf<PenaltyTicketEntity, PenaltyTicketDTO, Long> {
+
        @Autowired
        private PenaltyTicketRepo repo;
 
        @Autowired
        private PenaltyTicketMapping penaltyTicketMapping;
 
-
        // Lấy một đối tượng PenaltyTicketEntity theo giá trị
        // Input: penaltyTicketId (Long)
        // Output: PenaltyTicketEntity có giá trị penaltyTicketId tương ứng
-
-       public PenaltyTicketEntity getById (Long penaltyTicketId) {
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
+       public PenaltyTicketEntity getById(Long penaltyTicketId) {
               return this.repo.findByPenaltyTicketId(penaltyTicketId).orElse(null);
        }
 
        // Mapping đối tượng PenaltyTicketEntity --> PenaltyTicketDTO
        // Input: penaltyTicketId (Long)
        // Output: PenaltyTicketDTO có giá trị penaltyTicketId tương ứng
-
-       public PenaltyTicketDTO getById_toDTO (Long penaltyTicketId){
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
+       public PenaltyTicketDTO getById_toDTO(Long penaltyTicketId) {
               PenaltyTicketEntity penaltyTicketEntity = this.repo.findByPenaltyTicketId(penaltyTicketId).orElse(null);
 
-              if(penaltyTicketEntity != null){
+              if (penaltyTicketEntity != null) {
                      return this.penaltyTicketMapping.toDTO(penaltyTicketEntity);
               }
               return null;
@@ -45,24 +50,111 @@ public class PenaltyTicketService {
        // Lấy tất cả các đối tượng PenaltyTicketEntity
        // Input:
        // Output: List
-
-       public List<PenaltyTicketEntity> getAll () {
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
+       public List<PenaltyTicketEntity> getAll() {
               return this.repo.findAll();
        }
 
        // Mapping đối tượng List<PenaltyTicketEntity> --> List<PenaltyTicketDTO>
-
-       public List<PenaltyTicketDTO> getAll_toDTO () {
+       @Transactional(propagation = Propagation.REQUIRED, readOnly = true, isolation = Isolation.READ_COMMITTED)
+       @Override
+       public List<PenaltyTicketDTO> getAll_toDTO() {
               List<PenaltyTicketEntity> listPenaltyTicketEntities = this.repo.findAll();
 
               List<PenaltyTicketDTO> listPenaltyTicketDTOs = new ArrayList<>();
 
-              if(listPenaltyTicketEntities.isEmpty() == false){
-                     for (PenaltyTicketEntity e : listPenaltyTicketEntities){
+              if (listPenaltyTicketEntities.isEmpty() == false) {
+                     for (PenaltyTicketEntity e : listPenaltyTicketEntities) {
                             listPenaltyTicketDTOs.add(this.penaltyTicketMapping.toDTO(e));
                      }
                      return listPenaltyTicketDTOs;
               }
               return listPenaltyTicketDTOs;
        }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
+       @Override
+       public Boolean delete(Long id) {
+              
+              // kiem tra
+              Optional<PenaltyTicketEntity> optional = this.repo.findByPenaltyTicketId(id);
+
+              // neu co kq
+              if(optional.isPresent()){
+                     // xoa
+                     this.repo.delete(optional.get());
+                     return true;
+              }
+
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
+       @Override
+       public Boolean save(PenaltyTicketEntity entityObj) {
+              Optional<PenaltyTicketEntity> optional = this.repo.findByPenaltyTicketId(entityObj.getPenaltyTicketId());
+
+              // neu co kq
+              if(optional.isPresent() == false){
+                     // them
+                     this.repo.save(entityObj);
+                     return true;
+              }
+              
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
+       @Override
+       public Boolean save_toDTO(PenaltyTicketDTO dtoObj) {
+              Optional<PenaltyTicketEntity> optional = this.repo.findByPenaltyTicketId(dtoObj.getPenaltyTicketId());
+
+              // neu co kq
+              if(optional.isPresent() == false){
+
+                     // mapping
+                     PenaltyTicketEntity penaltyTicketEntity = this.penaltyTicketMapping.toEntity(dtoObj);
+
+                     // them
+                     this.repo.save(penaltyTicketEntity);
+                     return true;
+              }
+              
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ, rollbackFor = Exception.class)
+       @Override
+       public Boolean update(PenaltyTicketEntity entityObj) {
+              
+              Optional<PenaltyTicketEntity> optional = this.repo.findByPenaltyTicketId(entityObj.getPenaltyTicketId());
+
+              if(optional.isPresent()){
+                     // sua
+                     this.repo.save(entityObj);
+                     return true;
+              }
+
+              return false;
+       }
+
+       @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ, rollbackFor = Exception.class)
+       @Override
+       public Boolean update_toDTO(PenaltyTicketDTO dtoObj) {
+
+              Optional<PenaltyTicketEntity> optional = this.repo.findByPenaltyTicketId(dtoObj.getPenaltyTicketId());
+
+              if(optional.isPresent()){
+
+                     // mapping
+                     PenaltyTicketEntity penaltyTicketEntity = this.penaltyTicketMapping.toEntity(dtoObj);
+                     // sua
+                     this.repo.save(penaltyTicketEntity);
+                     return true;
+              }
+
+              return false;
+       }
+
 }
