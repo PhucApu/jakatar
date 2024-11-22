37bbb8219c8a0d9ed08025ea2e992de76770dd3a
thinh tu <khangthinhproc@gmail.com>
2024-11-22 15:31:04 +0700

    add error handling for services

--- Frontend/src/api/services/admin/employeeService.ts
+++ Frontend/src/api/services/admin/employeeService.ts
@@ -1,34 +1,47 @@
 import apiClient from "../../apiClient";
+import { apiRequest } from '../apiRequest';
 
 import { ApiResponse } from '@type/common/ApiResponse';
-import { Employee } from "@type/model/Employee";
+import { Employee } from '@type/model/Employee';
 
 export const getEmployees = async (): Promise<Employee[]> => {
-  const response = await apiClient.get<ApiResponse<Employee[]>>('/employees');
-  return response.data.data || [];
+  return apiRequest(async () => {
+    const response = await apiClient.get<ApiResponse<Employee[]>>('/employees');
+    return response.data.data || [];
+  });
 };
 
 export const getEmployeeById = async (employeeId: string): Promise<Employee> => {
-  const response = await apiClient.get<ApiResponse<Employee>>(`/employees/${employeeId}`);
-  return response.data.data!;
+  return apiRequest(async () => {
+    const response = await apiClient.get<ApiResponse<Employee>>(`/employees/${employeeId}`);
+    return response.data.data!;
+  });
 };
 
 export const createEmployee = async (employee: Partial<Employee>): Promise<Employee> => {
-  const response = await apiClient.post<ApiResponse<Employee>>('/employees/insert', employee);
-  return response.data.data!;
+  return apiRequest(async () => {
+    const response = await apiClient.post<ApiResponse<Employee>>('/employees/insert', employee);
+    return response.data.data!;
+  });
 };
 
 export const updateEmployee = async (employee: Partial<Employee>): Promise<Employee> => {
-  const response = await apiClient.put<ApiResponse<Employee>>('/employees/update', employee);
-  return response.data.data!;
+  return apiRequest(async () => {
+    const response = await apiClient.put<ApiResponse<Employee>>('/employees/update', employee);
+    return response.data.data!;
+  });
 };
 
 export const deleteEmployee = async (employeeId: string): Promise<Employee> => {
-  const response = await apiClient.delete<ApiResponse<Employee>>(`/employees/${employeeId}`);
-  return response.data.data!;
+  return apiRequest(async () => {
+    const response = await apiClient.delete<ApiResponse<Employee>>(`/employees/${employeeId}`);
+    return response.data.data!;
+  });
 };
 
 export const hideEmployee = async (employeeId: string): Promise<Employee> => {
-  const response = await apiClient.delete<ApiResponse<Employee>>(`/employees/hidden/${employeeId}`);
-  return response.data.data!;
-}
+  return apiRequest(async () => {
+    const response = await apiClient.delete<ApiResponse<Employee>>(`/employees/hidden/${employeeId}`);
+    return response.data.data!;
+  });
+};
