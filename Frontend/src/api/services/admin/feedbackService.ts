2bf5338512d8b724516f02db60c30ce6b6dbbdaf
DungTriNguyen <dungtringuyen113@gmail.com>
2024-12-06 16:30:39 +0700

    06/12 4:30PM send mail success

--- Frontend/src/api/services/admin/feedbackService.ts
+++ Frontend/src/api/services/admin/feedbackService.ts
@@ -45,3 +45,43 @@ export const hideFeedback = async (feedbackId: number): Promise<Feedback> => {
     return response.data.data!;
   });
 };
+
+
+
+// export const sendToEmailFeedback = async (
+//   toEmail: string,
+//   subject: string,
+//   body: string
+// ) => {
+//   return apiRequest(async () => {
+//     const response = await apiClient.post('/sendEmail', {
+//       toEmail,   // Truyền toEmail vào body
+//       subject,   // Truyền subject vào body
+//       body       // Truyền body vào body
+//     });
+//     console.log(">>DATA:", response.data.data);
+//     // return response.data.message || {}; // Trả về dữ liệu từ response
+//   });
+// };
+
+
+export const sendToEmailFeedback = async (
+  toEmail: string,
+  subject: string,
+  body: string
+): Promise<any> => {
+  return apiRequest(async () => {
+    // Sử dụng query parameters để gửi dữ liệu
+    const response = await apiClient.post('/sendEmail', null, {
+      params: {
+        toEmail,  // Truyền toEmail qua query params
+        subject,  // Truyền subject qua query params
+        body,     // Truyền body qua query params
+      },
+    });
+    console.log(">>DATA:", response.data);
+
+    // Trả về thông điệp từ backend
+    return response.data;
+  });
+};
