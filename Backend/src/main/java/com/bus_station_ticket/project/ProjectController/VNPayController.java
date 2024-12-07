4192c0a2c0c7259555440944bd4d469056526f0d
thinh tu <khangthinhproc@gmail.com>
2024-12-07 11:05:44 +0700

    vnpay

--- Backend/src/main/java/com/bus_station_ticket/project/ProjectController/VNPayController.java
+++ Backend/src/main/java/com/bus_station_ticket/project/ProjectController/VNPayController.java
@@ -1,8 +1,11 @@
 package com.bus_station_ticket.project.ProjectController;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import com.bus_station_ticket.project.ProjectDTO.TicketDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import com.bus_station_ticket.project.ProjectConfig.ResponseObject;
 import com.bus_station_ticket.project.ProjectService.TicketService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class VNPayController {
@@ -48,15 +52,44 @@ public class VNPayController {
               }
        }
 
+//       @GetMapping("/vnpay-payment-return")
+//       public ResponseEntity<ResponseObject> resultPayment(HttpServletRequest request) {
+//
+//              ResponseObject responseObject = this.ticketService.returnFronVNPay(request);
+//
+//              if (responseObject.getStatus().equals("success")) {
+//                     return  ResponseEntity.status(HttpStatus.OK).body(responseObject);
+//              }
+//              return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseObject);
+//
+//       }
+
        @GetMapping("/vnpay-payment-return")
-       public ResponseEntity<ResponseObject> resultPayment(HttpServletRequest request) {
+       public RedirectView resultPayment(HttpServletRequest request) {
 
               ResponseObject responseObject = this.ticketService.returnFronVNPay(request);
 
-              if (responseObject.getStatus().equals("success")) {
-                     return ResponseEntity.status(HttpStatus.OK).body(responseObject);
-              }
-              return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseObject);
+              List<TicketDTO> data = (List<TicketDTO>) responseObject.getData();
 
+              String returnUrl = "http://localhost:5173/export?status=";
+
+              if(responseObject.getStatus().equals("failure")) {
+                     returnUrl += responseObject.getStatus();
+              } else {
+                     List<String> listTicketId = new ArrayList<>();
+//                     List<String> listSeatNumber = new ArrayList<>();
+//                     String departureDate = data.get(0).getDepartureDate().toString();
+//                     String phoneNumber = data.get(0).getPhoneNumber();
+//                     String bookingUser = data.get(0).getAccountEnity_Id();
+
+                     data.forEach(ticketDTO ->  {
+                            listTicketId.add(ticketDTO.getTicketId().toString());
+//                            listSeatNumber.add(ticketDTO.getSeatNumber());
+                     });
+
+                     returnUrl += responseObject.getStatus() + "&ticketId=" + listTicketId.toString() ;
+              }
+
+           return new RedirectView(returnUrl);
        }
 }
